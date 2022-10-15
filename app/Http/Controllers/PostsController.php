8ca51f8ad490754111e462102b1450cc5df03ce8
sunshade88 <jasonsglim@gmail.com>
2022-10-15 16:26:11 +0800

    add pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,7 @@ class PostsController extends Controller
     public function index()
     {
         return view('blog.index', [
-            'posts' => Post::orderby('updated_at', 'desc')->get()
+            'posts' => Post::orderby('updated_at', 'desc')->paginate(10)
         ]);
     }
 
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -78,6 +78,10 @@
         </div>
     @endforeach
 
+    <div class="mx-auto pb-10 w-4/5">
+        {{ $posts->links() }}
+    </div>
+
 </body>
 
 </html>
